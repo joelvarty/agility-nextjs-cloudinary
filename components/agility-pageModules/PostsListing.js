7b246dfdbe7b09962c8555b77cd16b80fe96bb4f
Joel Varty <joel@agilitycms.com>
2021-06-08 17:52:31 -0400

    typo

--- components/agility-pageModules/PostsListing.js
+++ components/agility-pageModules/PostsListing.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { getImageObj, getOptimizedImage, nextImageLoader } from "../../lib/cloudinary-tools"
-import { isCompositeType } from "graphql";
+
 
 const PostsListing = ({ module, customData }) => {
 	// get posts
