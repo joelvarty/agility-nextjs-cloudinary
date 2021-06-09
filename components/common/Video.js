add6ca540144593c5201657b55f9cfbf2f3489b0
Joel Varty <joel@agilitycms.com>
2021-06-09 09:55:15 -0400

    added poster urls

--- components/common/Video.js
+++ components/common/Video.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { AdvancedVideo  } from '@cloudinary/react';
 import {Cloudinary} from "@cloudinary/base";
-
+import { getVideoPosterUrl } from "lib/cloudinary-tools"
 
 const Video = ({public_id, cloudName, className }) => {
 	const myCld = new Cloudinary({ cloud: {  cloudName } });
 	const video = myCld.video(public_id)
 
+	const posterUrl = getVideoPosterUrl({ public_id, cloud_name: cloudName })
+
 	 return (
-	 	<AdvancedVideo autoPlay={false} controls cldVid={video} className={className}  />
+	 	<AdvancedVideo autoPlay={false} controls cldVid={video} className={className}  poster={posterUrl} />
 	 )
 
 }
